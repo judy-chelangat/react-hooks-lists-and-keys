48b929c7ccacde3b0363788ca64e5c712fd135dd
judy-chelangat <judysigilai75@gmail.com>
2023-07-19 13:01:21 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ColorList from "./ColorList";
-
+function ColorItem(props){
+  return <li style={{color:props.color}}>{props.color}</li>
+}
 function App() {
+  const colors = [
+    "firebrick",
+    "rebeccapurple",
+    "salmon",
+    "darkslategray",
+    "hotpink",
+  ];
+const ColorEl = colors.map((color)=>{
+  return <li  key={color} style={{color: color}}>{color}</li>
+})
   return (
     <div>
-      <ColorList />
+      <h1>Top 5 CSS Colors</h1>
+      <ol>
+       {ColorEl}
+      </ol>
     </div>
   );
 }
 
 export default App;
+
